57a39c70a9814f5419cead2052f1103f694b327e
JerryTan <jryz101@gmail.com>
2019-07-16 17:22:33 +0800

    1. Imported Webkit framework, and declared a WKUIDelegate protocol.

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -1,20 +1,18 @@
-//
 //  ViewController.swift
 //  WKWebView
-//
 //  Created by Jerry Tan on 16/07/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
+
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
 }
 
